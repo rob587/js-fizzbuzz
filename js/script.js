7c1149d27f0c733e13b71b830730a13d09ac78ae
rob587 <robertocamm02@yahoo.com>
2025-07-02 17:44:26 +0200

    [add] else if else

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 
+
 //dichiarazione ciclo//
 for (let i = 1; i<=100; i++){
 
-    //statement condizionali//
+    //dichiarazione condizionale//
 
     if (i % 3 === 0 && i % 5 === 0){
         console.log("Fizzbuzz")
@@ -16,5 +17,14 @@ for (let i = 1; i<=100; i++){
         console.log("Fizz")
     }
 
+    else if(i % 5 ===0){
+        console.log("Buzz")
+    }
+
+   else {
+    console.log(i);
+  }
+
+  //terminata condizionale//
 
   }
