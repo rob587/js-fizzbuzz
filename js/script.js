20c5a419e7f80a4394d690b099e1f004bbf93536
rob587 <robertocamm02@yahoo.com>
2025-07-02 17:36:04 +0200

    aggiunte note

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@
 //ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-for (let i = 1; i<=100; ++i)
 
-    if(i%3===0){
+//dichiarazione ciclo//
+for (let i = 1; i<=100; i++){
+
+    //statement condizionali//
+
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("Fizzbuzz")
+    }
+
+    else if(i%3===0){
         console.log("Fizz")
     }
 
-    else if(i % 5 ===0){
-        console.log("Buzz")
-    }
+
+  }
