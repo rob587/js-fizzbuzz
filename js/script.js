ce77bc07a9cebe6fe61dab527139c8008477b53b
rob587 <robertocamm02@yahoo.com>
2025-07-02 17:20:43 +0200

    first if

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@
 //ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-for (let i = 1; i<=100; i++)
+for (let i = 1; i<=100; ++i)
+
+    if(i%3===0){
+        console.log("Fizz")
+    }
+
+    else if(i % 5 ===0){
+        console.log("Buzz")
+    }
